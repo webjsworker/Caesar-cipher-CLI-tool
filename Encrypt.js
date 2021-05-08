22db2a1d9490a90cbc9147027a47f880f61cb2e2
Alex <webjsworker@gmail.com>
2021-05-08 14:03:19 +0300

    update caesar-cipher

--- Encrypt.js
+++ Encrypt.js
@@ -1,25 +1,34 @@
 
-// принимает текст  и шаг сдвига
 
-
-
-
-/* console.log("Module work"); */
-
-/* let currentDate = new Date();
-module.exports.date = currentDate;
-
-module.exports.outTextfromCezar = function(value){
-    return "Good morning" + value;
-} */
-
-module.exports.encrypt = function(value,shift){
+module.exports.encrypt = function (value, shift) {
     var result = "";
     for (var i = 0; i <= value.length; i++) {
-        var symbol = value[i];
-        namber = value.charCodeAt(i) 
-        
-        result += String.fromCharCode(namber + shift )
-      }
-    return   result;
+       
+        namber = value.charCodeAt(i)
+
+        if (namber >= 65 && namber <= 90) {
+            for (var n = 1; n <= shift; n++) {
+                namber++
+                if (namber > 90) { namber = 65 }
+                if (namber < 65) { namber = 90 }
+            }
+            result += String.fromCharCode(namber)
+        } else if (namber >= 97 && namber <= 122) {
+            for (var n = 1; n <= shift; n++) {
+                namber++
+                if (namber > 122) { namber = 97 }
+                if (namber < 97) { namber = 122 }
+            }
+            result += String.fromCharCode(namber)
+        } else result += String.fromCharCode(namber)
+
+
+
+    }
+    return result;
 }
+
+
+
+
+

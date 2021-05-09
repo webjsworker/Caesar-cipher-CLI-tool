eca9510b28af31e3beee791582477795c1efab73
Alex <webjsworker@gmail.com>
2021-05-09 13:38:06 +0300

    write with add to string

--- Write.js
+++ Write.js
@@ -1,11 +1,19 @@
 let fs = require('fs')
 module.exports.write = function ( path_write, text ) {
     
-    let writeableStream = fs.createWriteStream(path_write);
-    let message = text
-    writeableStream.write(  message);
-    writeableStream.end("\n");
-    
+
+fs.appendFile(path_write, text , function (err) {
+  if (err) {
+    //
+  } else {
     console.log("Process is completed")
+   
+  }
+})
+
+
+    
+    
   return 
 }
+
